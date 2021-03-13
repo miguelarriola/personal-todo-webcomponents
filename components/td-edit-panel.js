f9ebc7255ec169f96d29cbe798d0de314ed1f73b
Miguel Arriola <miguelarriola007@gmail.com>
2021-03-13 15:39:24 -0600

    add edit panel behavior

--- components/td-edit-panel.js
+++ components/td-edit-panel.js
@@ -1,4 +1,4 @@
-import { setTask } from '../services/taskService.js';
+import taskService from '../services/taskService.js';
 
 const template = document.createElement('template');
 
@@ -15,13 +15,10 @@ template.innerHTML = `
       position: fixed;
       top: 0;
       width: 100%;
+      height: 100%;
       box-sizing: border-box;
       padding: var(--content-margin, 12px);
       background-color: var(--background);
-/*  */
-      background-color: aquamarine;
-      // box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
-/*  */
       outline: none;
       max-width: var(--app-max-width, 768px);
     }
@@ -38,18 +35,21 @@ template.innerHTML = `
       font-size: var(--icon-dimension-36); 
       -webkit-tap-highlight-color: transparent;
     }
-    .button:hover:not([disabled]){
+    .button:hover:not([data-disabled=true]){
       background-color: var(--light-grey);
       cursor: pointer;
     }
-    .button[disabled] { 
+    .button[data-disabled=true] { 
       color: var(--icon-disabled);
     }
     #text-field {
       width: 100%;
-      padding: 6px 0;
+      height: 40%;
+      padding: 0;
       border: none;
       outline: none;
+      overflow: auto;
+      resize: none;
       background-color: var(--background);
       font-family: var(--main-font);
       color: var(--color);
@@ -63,75 +63,134 @@ template.innerHTML = `
   </style>
   <div class="content">
     <div class="button-bar">
-      <i id="go-back" class="button material-icons">arrow_back</i>
-      <i id="confirm" class="button material-icons">delete_outline</i>
-      <i id="confirm" class="button material-icons" disabled>check</i>
+      <i id="go-back" class="button material-icons" title="Go back">arrow_back</i>
+      <i id="delete" class="button material-icons" title="Delete task">delete_outline</i>
+      <i id="confirm" class="button material-icons" title="Confirm changes">check</i>
     </div>
-    <textarea id="text-field" type="text" placeholder="New task" maxlength="240">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</textarea>
+    <textarea id="text-field" placeholder="Title task" maxlength="240"></textarea>
   </div>
 `;
 
 class EditPanel extends HTMLElement {
+  set taskId(value) {
+    this.dataset.taskId = value ? this.formatString(value) : '';
+  }
+
+  get taskId() {
+    return this.dataset.taskId.trim();
+  }
+
+  set taskTitle(value) {
+    this.textField.value = value ? this.formatString(value) : '';
+  }
+
+  get taskTitle() {
+    return this.formatString(this.textField.value);
+  }
+
+  set confirmButtonState(value) {
+    this.confirmButton.dataset.disabled = Boolean(value);
+  }
+
+  get confirmButtonState() {
+    return this.confirmButton.dataset.disabled === 'true';
+  }
+
+  static get observedAttributes() {
+    return ['data-task-id'];
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.textField = this.shadowRoot.querySelector('#text-field');
     this.goBackButton = this.shadowRoot.querySelector('#go-back');
+    this.deleteButton = this.shadowRoot.querySelector('#delete');
     this.confirmButton = this.shadowRoot.querySelector('#confirm');
-    this.inputForm = this.shadowRoot.querySelector('#input-form');
+    this.textField = this.shadowRoot.querySelector('#text-field');
+    this.onBuilding();
+  }
+
+  attributeChangedCallback() {
+    this.onTaskIdChanged();
   }
 
   connectedCallback() {
-    this.addEventListener('focus', this.onFocus);
-    this.textField.addEventListener('input', () => this.updateBtnState());
-    this.goBackButton.addEventListener('click', () => this.close());
+    this.goBackButton.addEventListener('click', () => this.onGoBack());
+    this.deleteButton.addEventListener('click', () => this.onDelete());
     this.confirmButton.addEventListener('click', () => this.onConfirm());
+    this.textField.addEventListener('input', () => this.onInput());
+    this.textField.addEventListener('keydown', (e) => this.onInputKeydown(e));
   }
 
   disconnectedCallback() {
-    this.removeEventListener('focus', this.onFocus);
-    this.textField.removeEventListener('input', this.updateBtnState);
-    this.goBackButton.removeEventListener('click', this.close);
+    this.goBackButton.removeEventListener('click', this.onGoBack);
+    this.deleteButton.removeEventListener('click', this.onDelete);
     this.confirmButton.removeEventListener('click', this.onConfirm);
+    this.textField.removeEventListener('input', this.onInput);
+    this.textField.removeEventListener('keydown', (e) =>
+      this.onInputKeydown(e)
+    );
   }
 
-  onFocus() {
-    this.textField.focus();
+  onBuilding() {
+    this.taskId = '';
+    this.taskTitle = '';
   }
 
-  close() {
-    this.clearTextField();
+  async onTaskIdChanged() {
+    const fetchedTask = await taskService.get(this.taskId);
+    this.taskTitle = fetchedTask?.title;
+    this.onInput();
+  }
+
+  onGoBack() {
+    this.onBuilding();
     this.hidden = true;
   }
 
-  updateBtnState(e) {
-    if (this.titleText) this.confirmButton.removeAttribute('disabled');
-    else this.confirmButton.setAttribute('disabled', '');
+  async onDelete() {
+    const deletedTask = await taskService.delete(this.taskId);
+    if (deletedTask)
+      this.notifyEvent('taskListModified', {
+        action: 'delete',
+        item: this.composeTask(deletedTask),
+      });
+    this.onGoBack();
   }
 
-  onConfirm() {
-    const title = this.titleText;
-    if (title) {
-      this.saveTask(title);
-      this.close();
+  async onConfirm() {
+    if (this.confirmButtonState) return;
+    const updatedTask = await taskService.update(this.taskId, {
+      title: this.taskTitle,
+    });
+    if (updatedTask) {
+      this.notifyEvent('taskListModified', {
+        action: 'update',
+        item: this.composeTask(updatedTask),
+      });
+      this.onGoBack();
     }
   }
 
-  clearTextField = () => {
-    this.textField.value = '';
-    this.updateBtnState();
-  };
-
-  get titleText() {
-    return this.textField.value.trim();
+  onInput() {
+    this.confirmButtonState = !Boolean(this.taskTitle);
+    this.textField.value = this.textField.value.replace(/(\r\n|\n|\r)/gm, ' ');
   }
 
-  saveTask = async (title) => {
-    await setTask({ title });
-    const taskAddedEvent = new CustomEvent('taskAdded', { bubbles: true });
-    this.dispatchEvent(taskAddedEvent);
-  };
+  onInputKeydown(e) {
+    if (e.key === 'Enter') this.onConfirm();
+  }
+
+  formatString = (string) =>
+    String(string)
+      .trim()
+      .replace(/(\r\n|\n|\r)/gm, ' ');
+
+  notifyEvent = (name, detail) =>
+    this.dispatchEvent(new CustomEvent(name, { detail }));
+
+  composeTask = ({ _id, title }) => ({ id: _id, title });
 }
 
 customElements.define('td-edit-panel', EditPanel);
--- services/taskService.js
+++ services/taskService.js
@@ -2,15 +2,23 @@ const host = 'https://todo-api-007.herokuapp.com';
 const resurce = 'tasks';
 const url = `${host}/${resurce}`;
 
+const TASK_ID_REGEX = /^[0-9a-z]{24}$/;
+
+const validId = (id) => TASK_ID_REGEX.test(id);
+
+const hasContent = (value) => value && String(value).trim();
+
 export const setTask = async (task) => {
   try {
-    return await fetch(url, {
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(task),
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    const addedTask = await response.json();
+    return addedTask;
     // window.location.href = 'index.html';
   } catch (error) {
     console.error(error);
@@ -19,33 +27,38 @@ export const setTask = async (task) => {
 
 export const getTasks = async () => {
   try {
-    const result = await fetch(url);
-    const tasks = await result.json();
-    return tasks;
+    const response = await fetch(url);
+    const fetchedTasks = await response.json();
+    return fetchedTasks;
   } catch (error) {
     console.error(error);
   }
 };
 
 export const getTask = async (id) => {
+  if (!validId(id)) return;
   try {
-    const result = await fetch(`${url}/${id}`);
-    const task = await result.json();
-    return task;
+    const response = await fetch(`${url}/${id}`);
+    const fetchedTask = await response.json();
+    return fetchedTask;
   } catch (error) {
     console.error(error);
   }
 };
 
-export const updateTask = async (task) => {
+export const updateTask = async (id, task) => {
+  if (!validId(id)) return;
+  if (!hasContent(task?.title)) return;
   try {
-    await fetch(`${url}/${task.id}`, {
+    const response = await fetch(`${url}/${id}`, {
       method: 'PUT',
-      body: JSON.stringify(task),
+      body: JSON.stringify({ title: task.title }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    const updatedTask = await response.json();
+    return updatedTask;
     // window.location.href = 'index.html';
   } catch (error) {
     console.error(error);
@@ -53,11 +66,22 @@ export const updateTask = async (task) => {
 };
 
 export const deleteTask = async (id) => {
+  if (!validId(id)) return;
   try {
-    await fetch(`${url}/${id}`, {
+    const response = await fetch(`${url}/${id}`, {
       method: 'DELETE',
     });
+    const deletedTask = await response.json();
+    return deletedTask;
   } catch (error) {
     console.error(error);
   }
+};
+
+export default {
+  set: setTask,
+  list: getTasks,
+  get: getTask,
+  update: updateTask,
+  delete: deleteTask,
 };
